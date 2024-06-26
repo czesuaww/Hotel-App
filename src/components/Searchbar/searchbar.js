466059b68da22b7ee66d443fb320d15596af6945
Grzegorz_Szwed <greg152295@gmail.com>
2024-06-26 20:56:19 +0200

    Added some function to check what is in searchbar

--- src/components/Searchbar/searchbar.js
+++ src/components/Searchbar/searchbar.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './searchbar.module.css';
 
 const SearchBar = () => {
+    const [term, setTerm] = useState('');
+
+    const search = () => console.log('szukaj', term);
+
+    const updateTerm = e => setTerm(e.target.value);
+
     return (
         <div className={styles.container}>
             <div className={styles.inputSearchContainer}>
                 <input
+                    value={term}
+                    onChange={updateTerm}
                     className={styles.inputSearch}
                     type="text"
                     placeholder="Szukaj..."
                 />
             </div>
             <div className={styles.buttonContainer}>
-                <button className={styles.searchButton} >Szukaj</button>
+                <button
+                    onClick={search}
+                    className={styles.searchButton}>
+                    Szukaj
+                </button>
             </div>
         </div>
     )
